b152545468d3495aebb6dc71f65b4c8f911b8e3f
usmanshoukat521 <usmanchoudhary521@gmail.com>
2020-12-01 19:41:15 +0500

    done

---
+++ assignment/e.java
@@ -0,0 +1,17 @@
+class e{
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		add(a,b);
+	}
+
+	public static int add(int a,int b){
+
+		int c=a+b;
+		System.out.println(c);
+		return c;
+	}
+
+}
