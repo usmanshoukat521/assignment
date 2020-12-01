91f510400a12280d40d5c731089de4b5f275b79a
seharafzal11 <seharafzal103@gmail.com>
2020-12-01 18:45:09 +0500

    done

---
+++ Assignment/a.java
@@ -0,0 +1,16 @@
+class a{
+
+
+public static void main(String[] args) {
+	
+	//decleration
+	int a;
+	//assignment
+	a=10;
+	//expression (initialization of b)
+	int b=a+5;
+	System.out.println(b);
+
+}
+
+}
---
+++ Assignment/b.java
@@ -0,0 +1,15 @@
+class b{
+
+	public static void main(String[] args) {
+		
+		int a=1;
+
+		if (a==1) {
+			
+			System.out.println("If Executed");
+		}
+
+		else
+			System.out.println("Else Executed");
+	}
+}
